aef458c0bd029c5530d4e320cc3996c3faa97619
samar-2004 <samarjunaid1994@gmail.com>
2025-09-10 20:55:27 +0500

    Fixed tailwind issue

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <h1 className="text-6xl font-bold text-center">Welcome to NextJS</h1>
+    <div className="w-full h-screen flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-bold text-center">Welcome to Trackly</h1>
       <p className="text-2xl text-center">
         Get started by editing <code className="text-sm">pages/index.js</code>
       </p>
